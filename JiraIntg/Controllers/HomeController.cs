811df4e0282c14da21d1d1d35c2f61cacd7791e1
nikhil.mohan <nikhil.mohan@experionglobal.com>
2019-04-17 14:41:51 +0530

    COIN-1 #comment backend logic updated

--- JiraIntg/Controllers/HomeController.cs
+++ JiraIntg/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace JiraIntg.Controllers
 
         public IActionResult About()
         {
-            //message to display
+            //message to display here
             ViewData["Message"] = "Your application description page.";
 
             return View();
