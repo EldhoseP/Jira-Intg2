67122fb46b664e58a82e107e0999b2a7bf49ea03
NiikhilMohanG <nikhil.g.mohan@outlook.com>
2019-05-03 18:52:49 +0530

    COIN-6 #comment code updated for testing

--- JiraIntg/Controllers/HomeController.cs
+++ JiraIntg/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace JiraIntg.Controllers
 
         public IActionResult About()
         {
-            //message to display add here test
+            //message to display add here test integration
             ViewData["Message"] = "Your application description page.";
 
             return View();
